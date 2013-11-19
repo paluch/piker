3d6acff18ef16b63fe234c21208985e8a7418004
paluch <paluch.tiago@gmail.com>
2013-11-19 16:19:15 -0200

    Initial Commit

---
+++ pikerTests/pikerTests.m
@@ -0,0 +1,34 @@
+//
+//  pikerTests.m
+//  pikerTests
+//
+//  Created by Tiago Paluch on 19/11/13.
+//  Copyright (c) 2013 Tiago Paluch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface pikerTests : XCTestCase
+
+@end
+
+@implementation pikerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
